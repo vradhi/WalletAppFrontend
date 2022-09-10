ee557a33c6626758f0279b8f47428cf9fd8e8269
unknown <vradhis22@gmail.com>
2022-09-10 12:45:05 +0530

    added wallet module and tested api for its creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navigation from './components/shared/Navigation';
+import Welcome from './components/Welcome';
+import Dashboard from './components/dashboard/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CreateWallet from './components/dashboard/operations/CreateWallet';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+     <Router>
+       
+       <Routes>
+       {/* <Route path="/" element={<Welcome />} /> */}
+       <Route path="/" element={<Navigation />} /> 
+          <Route path="/" exact element={<Welcome />} />
+         <Route path="/dashboard" exact element={<Dashboard />} />
+         <Route path="/createwallet" exact element={<CreateWallet />} />
+      
+         {/* <Route path="/CreateWallet" exact component={CreateWallet} />  */}
+       </Routes>
+    
+     </Router>
+    
   );
 }
 
---
+++ src/components/Welcome.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+class Welcome extends Component
+ {
+     render() {
+         return (
+             <div className="landing">
+                 <div className="light-overlay landing-inner text-dark">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <h1 className="display-3 mb-4">Personal Expense Manager</h1>
+                        <p className="lead">
+                            Create your account to manage your daily expense and hisaab kitaab!
+                        </p>
+                        <hr />
+                        <a href="register.html" className="btn btn-lg btn-primary mr-2">
+                            Sign Up
+                        </a>
+                        <p></p>
+                        <a href="login.html" className="btn btn-lg btn-secondary mr-2">
+                            Login
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+             </div>
+         )
+     }
+ }
+
+ export default Welcome;
